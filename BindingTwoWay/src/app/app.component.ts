bdaf94ee4834811839216cb315a437215ff9b941
PranaliMohite0408 <pranalimohite482000@gmail.com>
2022-11-25 21:20:33 +0530

    Angular Project

---
+++ BindingTwoWay/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent
+{
+ 
+ /* public str = "Marvellous";
+ 
+ public value:any;
+
+ public Send(data:any)
+ {
+    this.value = data;
+ }*/
+
+ public Name:any;
+
+ 
+}
